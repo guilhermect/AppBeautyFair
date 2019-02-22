a0be6baaf850177023d4303dd01326945eb8d885
Guilherme Costa <guilhermect1999@gmail.com>
2019-02-22 00:23:22 -0300

    Updating header buttons

---
+++ painel/application/controllers/Painel.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Painel extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+		
+		$this->load->helper('url');
+	}
+		
+	public function index()
+	{
+		$this->load->view('painel/frontend/html-header');
+		$this->load->view('painel/frontend/header');
+		$this->load->view('painel/index');
+		$this->load->view('painel/frontend/footer');
+		$this->load->view('painel/frontend/html-footer');
+	}
+}
