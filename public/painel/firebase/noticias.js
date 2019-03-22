bb02bd8a9b6aa824fcd3d3b4569c533e0ab6a1ad
Guilherme Costa <guilhermect1999@gmail.com>
2019-03-22 11:12:17 -0300

    Update noticias.js

--- public/painel/firebase/noticias.js
+++ public/painel/firebase/noticias.js
@@ -2,42 +2,7 @@ const userList = document.querySelector('#user-list');
 const form = document.querySelector('#add-user-form');
 const formUpd = document.querySelector('#update-user-form');
 
-//Upload File
 
-var uploader = document.getElementById('uploader');
-var fileButton = document.getElementById('fileButton');
-
-//Listen for file selection
-fileButton.addEventListener('change', function(e){
-    //Get file
-    var file = e.target.files[0];
-
-    //Create storage ref
-    var storageRef = firebase.storage().ref('news/' + file.name);
-
-
-    //Upload file
-    var task = storageRef.put(file);
-
-    //update progress bar
-    task.on('state_changed', 
-
-        function progress(snapshot){
-            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            uploader.value = percentage;
-
-        },
-
-        function error(err){
-
-        },
-
-        function complete(){
-
-        }
-
-    );
-});
 
 // Create element and render user
 function renderUser(doc){
@@ -184,6 +149,42 @@ db.collection('news').doc('hair').collection('items').onSnapshot(snapshot => {
     })
 })*/
 
+//Upload File
+
+var uploader = document.getElementById('uploader');
+var fileButton = document.getElementById('fileButton');
+
+//Listen for file selection
+fileButton.addEventListener('change', function(e){
+    //Get file
+    var file = e.target.files[0];
+
+    //Create storage ref
+    var storageRef = firebase.storage().ref('news/' + file.name);
+
+
+    //Upload file
+    var task = storageRef.put(file);
+
+    //update progress bar
+    task.on('state_changed', 
+
+        function progress(snapshot){
+            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploader.value = percentage;
+
+        },
+
+        function error(err){
+
+        },
+
+        function complete(){
+
+        }
+
+    );
+});
 
 
 
